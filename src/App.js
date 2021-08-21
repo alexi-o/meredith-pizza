e3a908f4d8b675fa3cb79e5ba020a129f4fb55f1
Alexi OHearn <ohearnalexi@gmail.com>
2021-08-20 18:43:55 -0600

    adds pizza greatness and firebase config settings

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="grid"></div>
+      <div className="lines"></div>
+      <h1>
+        <span>MEREDITH</span>
+        <span>MEREDITH</span>
+      </h1>
+      <h2>Pizza</h2>
+
+      <div className="container">
+        <div className="crust">
+            <div className="pep"></div>
+            <div className="face">
+                <div className="eye"></div>
+                <div className="mouth"></div>
+            </div>
+        </div>
+        <div className="melt"></div>
+    </div>
     </div>
   );
 }
